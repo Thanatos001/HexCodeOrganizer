23582698cf3560a5add555738a69211a0fdd0ace
Thanatos001 <74594872+Thanatos001@users.noreply.github.com>
2024-07-01 13:06:03 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,29 @@
-self.addEventListener('install', (event) => {
+const CACHE_NAME = 'hex-code-organizer-cache';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+];
+
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open('hex-organizer-v1').then((cache) => {
-            return cache.addAll([
-                './',
-                './index.html',
-                './style.css',
-                './script.js',
-                './manifest.json'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
     );
 });
